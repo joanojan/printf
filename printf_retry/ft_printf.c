2fccde899d5003de8b52df12e52e2fa7a7c2bcdb
Joan Vila Valls <jvila-va@car4s3.42barcelona.com>
2025-06-11 11:02:37 +0200

    logicchange

--- printf_retry/ft_printf.c
+++ printf_retry/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jvila-va <jvila-va@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 09:31:39 by jvila-va          #+#    #+#             */
-/*   Updated: 2025/06/11 10:08:47 by jvila-va         ###   ########.fr       */
+/*   Updated: 2025/06/11 10:48:14 by jvila-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,20 @@ static int	handle_format_specifier(const char c, va_list args)
 {
 	if (c == 's')
 		return (ft_print_va_str(args));
-	if (c == 'c')
+	else if (c == 'c')
 		return (ft_print_va_chr(args));
-	if (c == 'd' || c == 'i')
+	else if (c == 'd' || c == 'i')
 		return (ft_print_va_int(args));
-	if (c == 'u')
+	else if (c == 'u')
 		return (ft_print_va_uint(args));
-	if (c == 'p')
+	else if (c == 'p')
 		return (ft_print_va_ptr(args));
-	if (c == 'x' || c == 'X')
+	else if (c == 'x' || c == 'X')
 		return (ft_print_va_hex(args, c));
-	if (c == '%')
-		return (ft_putchar_fd(c, STDOUT_FILENO));
-	return (-1);
-}
-
-int	static	handle_non_specifier(char c)
-{
-	int	printed;
-
-	printed = 0;
-	printed = ft_putchar_fd('%', STDOUT_FILENO);
-	printed += ft_putchar_fd(c, STDOUT_FILENO);
-	return (printed);
+	else if (c == '%')
+		return (ft_putchar_fd('%', STDOUT_FILENO));
+	else
+		return (ft_putchar_fd('%', 1) + ft_putchar_fd(c, 1));
 }
 
 /*	write the output under the control of a format string  that specifies
@@ -59,10 +50,8 @@ int	ft_printf(const char *fmt, ...)
 		if (*fmt == '%')
 		{
 			fmt++;
-			if (ft_strchr(CONVERSION, *fmt))
+			if (*fmt)
 				printed = handle_format_specifier(*fmt, args);
-			else
-				printed = handle_non_specifier(*fmt);
 		}
 		else
 			printed = ft_putchar_fd(*fmt, STDOUT_FILENO);
